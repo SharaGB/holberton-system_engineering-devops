c2e71648fe37c29088b7870d94cce6040b358ef9
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-22 15:19:28 -0500

    Fix code fail

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,21 @@
 # Install and configure an Nginx server using Puppet
 
 package { 'nginx':
-  ensure => installed,
+  ensure => 'installed'
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+file_line { 'redirection-301':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
-}
-
-file { '/var/www/html/index.nginx-debian.html':
-  content => 'Hello World',
-}
-
-file_line { 'redirection-301':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => '/server_name _;/ a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }

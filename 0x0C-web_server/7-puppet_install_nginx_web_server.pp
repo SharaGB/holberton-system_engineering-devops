414cee125276301281f9551a5616322040e8e005
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-21 22:45:29 -0500

    Fix install and configure an Nginx server using Puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,7 +14,7 @@ file { '/var/www/html/index.nginx-debian.html':
 }
 
 file_line { 'redirection-301':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',

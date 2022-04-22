a477df51daf55c794b7060a0457620084ac2f3d5
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-22 15:24:36 -0500

    Fix code fail

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,6 +4,11 @@ package { 'nginx':
   ensure => installed
 }
 
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
 file { '/var/www/html/index.html':
   content => 'Hello World',
 }
@@ -13,9 +18,4 @@ file_line { '/redirect_me 301':
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
 }

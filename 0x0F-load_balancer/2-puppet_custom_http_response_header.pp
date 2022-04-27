23b40bdb683f6459a75ce2edbae0bc90ff584a75
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-27 13:38:05 -0500

    Fix a custom HTTP header response with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,37 @@
 # Add a custom HTTP header with Puppet
-exec { 'X-Served-By':
-  command  => 'sudo apt-get update;
-                sudo apt-get -y install nginx;
-                echo "Hello World" > /var/www/html/index.nginx-debian.html
-                sudo sed -i "/server_name _;/ a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default
-                sudo sed -i "/server_name _;/ a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
-                service nginx restart',
-  provider => shell,
+# exec { 'X-Served-By':
+#  command  => 'sudo apt-get update;
+#                sudo apt-get -y install nginx;
+#                echo "Hello World" > /var/www/html/index.nginx-debian.html
+#                sudo sed -i "/server_name _;/ a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default
+#                sudo sed -i "/server_name _;/ a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+#                service nginx restart',
+#  provider => shell,
+# }
+
+package { 'nginx':
+  ensure => installed
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+file_line { '/redirect_me 301':
+  ensure => present,
+  after  => 'listen 80 default_server;',
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file_line { 'X-Served-By':
+  ensure => present,
+  after  => 'listen 80 default_server;',
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'add_header X-Served-By $HOSTNAME;"',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }

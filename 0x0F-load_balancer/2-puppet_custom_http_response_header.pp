0ab15b7175d29de34c672f18d0fe9f458e4d37c5
SharaGB <shara.garcia.betancur@gmail.com>
2022-04-27 13:26:41 -0500

    Creating a custom HTTP header response, but with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,10 @@
 # Add a custom HTTP header with Puppet
-file_line {'X-Served-By':
-    after => 'server_name _;',
-    path  => '/etc/nginx/sites-available/default',
-    line  => 'add_header X-Served-By $HOSTNAME',
+exec { 'X-Served-By':
+  command  => 'sudo apt-get update;
+                sudo apt-get -y install nginx;
+                echo "Hello World" > /var/www/html/index.nginx-debian.html
+                sudo sed -i "/server_name _;/ a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default
+                sudo sed -i "/server_name _;/ a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+                service nginx restart',
+  provider => shell,
 }

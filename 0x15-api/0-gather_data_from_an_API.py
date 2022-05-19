57eff7d5f0725510f8690dcadfde328170688024
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-19 12:34:08 -0500

    Fix returns information about his/her list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,22 +2,45 @@
 """ Using this REST API, for a given employee ID, returns
 information about his/her TODO list progress. """
 from requests import get
-from sys import argv
+from sys import argv, exit
 
 if __name__ == "__main__":
-    """list task to Employees"""
-    id = int(argv[1])
-    user = get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(id)).json()
-    todos = get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'
-        .format(id)).json()
+    try:
+        id = argv[1]
+        is_integer = int(id)
+    except Exception:
+        exit()
 
-    tareas = []
-    for task in todos:
-        if task.get('completed') is True:
-            tareas.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}): ".
-          format(user.get('name'), len(tareas), len(todos)))
-    for task in tareas:
-        print("\t {}".format(task))
+    url = "https://jsonplaceholder.typicode.com/"
+    url_user = url + "users?id=" + id
+    url_todo = url + "todos?userId=" + id
+
+    request_user = get(url_user)
+    request_todo = get(url_todo)
+    # Connection and have an access to the json
+    try:
+        jsuser = request_user.json()
+        jstodo = request_todo.json()
+    except ValueError:
+        print("No Json")
+
+    # Assign values
+    if jsuser and jstodo:
+        EMPLOYEE_NAME = jsuser[0].get('name')
+        NUMBER_OF_DONE_TASKS = 0
+        for task in jstodo:
+            if task.get('completed'):
+                NUMBER_OF_DONE_TASKS += 1
+        TOTAL_NUMBER_OF_TASKS = len(jstodo)
+
+        # Print first line
+        print("Employee {} is done with tasks({}/{}):"
+              .format(EMPLOYEE_NAME,
+                      NUMBER_OF_DONE_TASKS,
+                      TOTAL_NUMBER_OF_TASKS))
+
+        # Second and N lines
+        for doing in jstodo:
+            TASK_TITLE = doing.get('title')
+            if doing.get('completed'):
+                print("\t {}".format(TASK_TITLE))

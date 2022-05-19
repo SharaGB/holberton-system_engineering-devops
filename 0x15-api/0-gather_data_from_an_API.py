8b1d2fb68d94508ea076fe5035fde9914c6c93e4
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-19 12:33:15 -0500

    Fix returns information about his/her list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,27 +4,20 @@ information about his/her TODO list progress. """
 from requests import get
 from sys import argv
 
-if __name__ == '__main__':
-    """ Returns information about his/her TODO list progress """
-    id = int(argv[1])  # Is the employee ID
-    todos = get('https://jsonplaceholder.typicode.com/todos?userId={}'.
-                format(id)).json()
-    users = get(
+if __name__ == "__main__":
+    """list task to Employees"""
+    id = int(argv[1])
+    user = get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(id)).json()
+    todos = get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(id)).json()
 
-    if users and todos:
-        EMPLOYEE_NAME = users.get('name')  # Name of the employee
-        NUMBER_OF_DONE_TASKS = 0  # Number of completed tasks
-        for tasks in todos:
-            if tasks.get('completed') is True:
-                # Total number of tasks
-                NUMBER_OF_DONE_TASKS += 1
-        TOTAL_NUMBER_OF_TASKS = len(todos)
-        # First line: Employee
-        print("Employee {} is done with tasks({}/{}):".format(
-            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-        # Second and N next lines display the title of completed tasks
-        for tasks in todos:
-            TASK_TITLE = tasks.get('title')
-            if tasks.get('completed') is True:
-                print(f"\t {TASK_TITLE}")
+    tareas = []
+    for task in todos:
+        if task.get('completed') is True:
+            tareas.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}): ".
+          format(user.get('name'), len(tareas), len(todos)))
+    for task in tareas:
+        print("\t {}".format(task))

3f32b1a80212fbfbd1a5890f889c04d236808d63
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-19 15:44:28 -0500

    Fix records all tasks from all employees

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,21 +6,16 @@ from requests import get
 if __name__ == '__main__':
     users = get('https://jsonplaceholder.typicode.com/users/').json()
     todos = get('https://jsonplaceholder.typicode.com/todos/').json()
-    user_dict = {}
-    name_dict = {}
-    for tasks in users:
-        USERNAME = tasks.get('username')
-        USER_ID = tasks.get('id')
-        user_dict[USER_ID] = []
-        name_dict[USER_ID] = USERNAME
+    data = {}
+    for user in users:
+        list_employees = []
+        for progress in todos:
+            USERNAME = user.get('username')
+            TASK_TITLE = progress.get('title')
+            TASK_COMPLETED_STATUS = progress.get('completed')
+            list_employees.append({"username": USERNAME, "task": TASK_TITLE,
+                                   "completed": TASK_COMPLETED_STATUS})
+        data[user['id']] = list_employees
 
-    for progress in todos:
-        userId = progress.get('userId')
-        USERNAME = name_dict.get(userId)
-        TASK_TITLE = progress.get('title')
-        TASK_COMPLETED_STATUS = progress.get('completed')
-        dict = {"username": USERNAME, "task": TASK_TITLE,
-                "completed": TASK_COMPLETED_STATUS}
-        user_dict.get(userId).append(dict)
     with open('todo_all_employees.json', 'w') as jsonfile:
-        json.dump(user_dict, jsonfile)
+        json.dump(data, jsonfile)

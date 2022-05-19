548b3fbee913f73b1116699d449fdac01c853531
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-19 12:50:00 -0500

    Fix returns information about his/her list progress

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,25 +5,25 @@ from requests import get
 from sys import argv
 
 if __name__ == '__main__':
-    """ Returns information about his/her TODO list progress """
     id = int(argv[1])  # Is the employee ID
     users = get(f'https://jsonplaceholder.typicode.com/users/{id}').json()
     todos = get(f'https://jsonplaceholder.typicode.com/todos?userId={id}').\
         json()
 
-    if users and todos:
-        EMPLOYEE_NAME = users.get('name')  # Name of the employee
-        NUMBER_OF_DONE_TASKS = 0  # Number of completed tasks
-        for progress in todos:
-            if progress.get('completed') is True:
-                # Total number of tasks
-                NUMBER_OF_DONE_TASKS += 1
+    EMPLOYEE_NAME = users.get('name')  # Name of the employee
+    NUMBER_OF_DONE_TASKS = 0  # Number of completed tasks
+    for progress in todos:
+        if progress.get('completed'):
+            # Total number of tasks
+            NUMBER_OF_DONE_TASKS += 1
         TOTAL_NUMBER_OF_TASKS = len(todos)
-        # First line: Employee
-        print(f"Employee {EMPLOYEE_NAME} is done ", end='')
-        print(f"with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-        # Second and N next lines display the title of completed tasks
-        for tasks in todos:
-            TASK_TITLE = tasks.get('title')
-            if tasks.get('completed') is True:
-                print(f"\t {TASK_TITLE}")
+    # First line: Employee
+    print("Employee {} is done with tasks({}/{}):"
+          .format(EMPLOYEE_NAME,
+                  NUMBER_OF_DONE_TASKS,
+                  TOTAL_NUMBER_OF_TASKS))
+    # Second and N next lines display the title of completed tasks
+    for tasks in todos:
+        TASK_TITLE = tasks.get('title')
+        if tasks.get('completed') is True:
+            print("\t {}".format(TASK_TITLE))

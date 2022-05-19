7faac7daa0accd81062d7b7ea92e58e2388390f9
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-19 15:49:50 -0500

    Fix records all tasks from all employees

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,17 +5,19 @@ from requests import get
 
 if __name__ == '__main__':
     users = get('https://jsonplaceholder.typicode.com/users/').json()
-    todos = get('https://jsonplaceholder.typicode.com/todos/').json()
     data = {}
-    for user in users:
+    for username in users:
         list_employees = []
+        USER_ID = {"userId": username["id"]}
+        todos = get('https://jsonplaceholder.typicode.com/todos/',
+                    params=USER_ID).json()
         for progress in todos:
-            USERNAME = user.get('username')
+            USERNAME = username.get('username')
             TASK_TITLE = progress.get('title')
             TASK_COMPLETED_STATUS = progress.get('completed')
             list_employees.append({"username": USERNAME, "task": TASK_TITLE,
                                    "completed": TASK_COMPLETED_STATUS})
-        data[user['id']] = list_employees
+        data[username['id']] = list_employees
 
     with open('todo_all_employees.json', 'w') as jsonfile:
         json.dump(data, jsonfile)

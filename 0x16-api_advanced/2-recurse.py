0aeed2d6e02b20dbb51835239749087c553be85b
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-22 21:52:17 -0500

    Recursive function that returns a list containing the titles

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,18 +6,16 @@ from requests import get
 
 def recurse(subreddit, hot_list=[], after=None):
     """ Recursively """
-    URL = "https://www.reddit.com/r/{}/hot.json?after={}".format(
-        subreddit, after)
-    r = get(URL, headers={'user-agent': 'MyAPI'})
+    URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    r = get(URL, params={"after": after}, headers={"User-agent": "holbie"})
     if r.status_code == 200:
-        data = r.json()
+        data = data.json()
         posts = data['data']['children']
         after = data['data']['after']
-        for title in posts:
-            hot_list.append(title['data']['title'])
-            if after is not None:
-                return recurse(subreddit, hot_list, after)
-            else:
-                return hot_list
+        if after is not None:
+            hot_list.append(posts[0]['data']['title'])
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
     else:
         return None

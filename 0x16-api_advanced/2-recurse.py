ebd827865627a49af1818ac651ac37124bf6172d
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-22 21:43:26 -0500

    Recursive function that returns a list containing the titles of all hot articles

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Returns a list containing the titles of all hot
+        articles for a given subreddit """
+from requests import get
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Recursively """
+    URL = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+        subreddit, after)
+    r = get(URL, headers={'user-agent': 'MyAPI'})
+    if r.status_code == 200:
+        data = r.json()
+        posts = data['data']['children']
+        after = data['data']['after']
+        for title in posts:
+            hot_list.append(title['data']['title'])
+            if after is not None:
+                return recurse(subreddit, hot_list, after)
+            else:
+                return hot_list
+    else:
+        return None

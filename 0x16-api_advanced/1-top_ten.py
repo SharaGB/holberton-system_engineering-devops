fc07bd3f41a6c51ae8a014b235252ca410f2bb64
SharaGB <shara.garcia.betancur@gmail.com>
2022-05-22 21:26:43 -0500

    Print the titles of the first 10 hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ from requests import get
 def top_ten(subreddit):
     """ Returns the tittles of the first 10 hot posts """
     URL = "https://www.reddit.com/r/{}/.json?limit=10".format(subreddit)
-    r = get(URL, headers={'user-agent': 'MyAPI/0.0.1'})
+    r = get(URL, headers={'user-agent': 'MyAPI'})
     if r.status_code == 200:
         data = r.json()
         posts = data['data']['children']
